7b3bcc07d28c26cb8821cd4549a983a7b1e36732
gili-julio <giliardoplay@gmail.com>
2025-01-11 22:12:59 -0300

    refactor: remove resizable of the window

--- src/main/java/br/ufrn/imd/controller/ScreenManager.java
+++ src/main/java/br/ufrn/imd/controller/ScreenManager.java
@@ -3,6 +3,7 @@ package br.ufrn.imd.controller;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -13,13 +14,20 @@ public class ScreenManager {
     @SuppressWarnings("exports")
 	public static void setPrimaryStage(Stage stage) {
         primaryStage = stage;
+        primaryStage.setResizable(false);
     }
 
     public static void switchScreen(String fxmlFile, String sceneTitle) {
         try {
             FXMLLoader loader = new FXMLLoader(ScreenManager.class.getResource(fxmlFile));
             Parent root = loader.load();
-            primaryStage.setScene(new Scene(root));
+            // Criar um BorderPane e centralizar o conteúdo na posição CENTER
+            BorderPane borderPane = new BorderPane();
+            borderPane.setCenter(root);
+
+            // Criar a cena e configurar o tamanho
+            Scene scene = new Scene(borderPane);
+            primaryStage.setScene(scene);
             primaryStage.setTitle(sceneTitle);
             primaryStage.show();
         } catch (IOException e) {
