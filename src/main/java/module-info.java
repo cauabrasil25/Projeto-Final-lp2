02fc4956eb4840bbf52063b72a422f59171c3e87
gili-julio <giliardoplay@gmail.com>
2025-01-13 00:38:05 -0300

    fix: restaura o module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module br.ufrn.imd.projeto_final {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires java.logging;
+
+
+    opens br.ufrn.imd.view to javafx.fxml;
+    opens br.ufrn.imd.controller to javafx.fxml;
+
+    exports br.ufrn.imd.model;
+    exports br.ufrn.imd.dao;
+    exports br.ufrn.imd.view;
+    exports br.ufrn.imd.controller;
+}
